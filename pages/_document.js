f4cc201fd93a539dc53d1841c5fc0baa1b7d6df6
mustaphasa <mostaphasa@gmail.com>
2023-09-02 21:13:05 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -26,13 +26,18 @@ class MyDocument extends Document {
           <script data-ad-client="YOUR_AD_CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
-          {/* Add your text content here */}
           <header>
             <h1>Welcome to Thumbnail Website</h1>
             <p>By using this tool, you can easily get thumbnails from YouTube videos and then download them for free. This service works for computers, tablets, and mobile devices.</p>
           </header>
           <Main />
           <NextScript />
+          
+          {/* Add your text content here */}
+          <div className="bg-gray-200 p-4">
+            <p>It will always find the thumbnails in the highest quality and with max resolution.</p>
+            <p>To get the thumbnail, you need to copy the YouTube video URL into our tool.</p>
+          </div>
         </body>
       </Html>
     );
