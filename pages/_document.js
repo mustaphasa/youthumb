00ff696a593bad42bdac217fe13acb7a28744bdf
mustaphasa <mostaphasa@gmail.com>
2023-09-02 20:58:06 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -5,45 +5,33 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Add meta description and title for SEO */}
-          <meta name="description" content="Your website description goes here." />
-          <title>Youtubedownload-thumbnail</title>
-
-          {/* Add favicon */}
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-
-          {/* Add AdSense script */}
-          <script
-            data-ad-client="YOUR_AD_CLIENT_ID"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script>
+          <script data-ad-client="YOUR_AD_CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
-        <body className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-sans">
-          {/* Header Section */}
-          <header className="bg-blue-700 text-white p-4">
-            <div className="mb-4">
-              <h1 className="text-4xl font-bold">Welcome to Thumbnail Website</h1>
-              <p className="text-lg">By using this tool, you can easily get thumbnails from YouTube videos and then download them for free. This service works for computers, tablets, and mobile devices.</p>
-            </div>
+        <body>
+          {/* Add your text content here */}
+          <header>
+            <h1>Welcome to My Next.js Website</h1>
+            <p>This is some introductory text for your website.</p>
           </header>
-
-          {/* Main Content */}
-          <main className="container mx-auto p-4">
-            {/* Add your main content here */}
-            {/* Example: */}
-            <h2 className="text-2xl font-bold mb-4"></h2>
-            <p className="text-lg">.</p>
-          </main>
-
-          {/* Footer Section */}
-          <footer className="bg-gray-900 text-white text-center p-4">
-            &copy; {new Date().getFullYear()} youtubedownload-thumbnail
-          </footer>
-
+          <Main />
           <NextScript />
         </body>
       </Html>
@@ -52,5 +40,6 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
 
 
