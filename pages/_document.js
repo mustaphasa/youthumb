bd4bec95847bdb5c1811e85d5c0e55bcede208ae
mustaphasa <mostaphasa@gmail.com>
2023-09-02 21:23:09 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -5,39 +5,41 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
+          {/* Add meta description and title for SEO */}
+          <meta name="description" content="Download high-quality thumbnails from YouTube videos." />
+          <title>Youtube Thumbnail Downloader</title>
+
+          {/* Add favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+
+          {/* Add AdSense script */}
           <script data-ad-client="YOUR_AD_CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
-          <header>
-            <h1>Welcome to Thumbnail Website</h1>
-            <p>By using this tool, you can easily get thumbnails from YouTube videos and then download them for free. This service works for computers, tablets, and mobile devices.</p>
+          {/* Header Section */}
+          <header className="bg-blue-500 text-white p-4">
+            <h1 className="text-2xl font-bold">Youtube Thumbnail Downloader</h1>
+            <p>Download high-quality thumbnails from YouTube videos.</p>
           </header>
+
+          {/* Main Content */}
           <Main />
-          <NextScript />
-          
-          {/* Add your text content here */}
-          <div className="bg-gray-200 p-4">
+
+          {/* Footer Section */}
+          <footer className="bg-gray-800 text-white text-center p-4">
+            &copy; {new Date().getFullYear()} Youtube Thumbnail Downloader
+          </footer>
+
+          {/* Additional Text Content */}
+          <div className="bg-gray-200 p-4 text-center">
             <p>It will always find the thumbnails in the highest quality and with max resolution.</p>
             <p>To get the thumbnail, you need to copy the YouTube video URL into our tool.</p>
           </div>
+
+          <NextScript />
         </body>
       </Html>
     );
