d4767b33933d440e3032dfdb33bf288dbb1d72cb
mustaphasa <mostaphasa@gmail.com>
2023-09-02 19:19:40 +0100

    adding adsens

--- pages/_document.js
+++ pages/_document.js
@@ -5,33 +5,43 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
+          {/* Add meta description and title for SEO */}
+          <meta name="description" content="Your website description goes here." />
+          <title>Your Website Title</title>
+
+          {/* Add favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <script data-ad-client="YOUR_AD_CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+          {/* Add AdSense script */}
+          <script
+            data-ad-client="YOUR_AD_CLIENT_ID"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
         </Head>
-        <body>
-          {/* Add your text content here */}
-          <header>
-            <h1>Welcome to My Next.js Website</h1>
-            <p>This is some introductory text for your website.</p>
+        <body className="bg-gray-100 font-sans">
+          {/* Header Section */}
+          <header className="bg-blue-500 text-white p-4">
+            <h1 className="text-2xl font-bold">Welcome to Thumbnail Website</h1>
+            <p>By using this tool you can easily get thumbnails from YouTube videos and then download them for free. This service works for computer, tablets, and mobile devices.</p>
           </header>
-          <Main />
+
+          {/* Main Content */}
+          <main className="container mx-auto p-4">
+            {/* Add your main content here */}
+            {/* Example: */}
+            <h2 className="text-xl font-bold mb-4">Main Content Section</h2>
+            <p>Here is the main content of your website.</p>
+          </main>
+
+          {/* Footer Section */}
+          <footer className="bg-gray-800 text-white text-center p-4">
+            &copy; {new Date().getFullYear()} Your Website Name
+          </footer>
+
           <NextScript />
         </body>
       </Html>
