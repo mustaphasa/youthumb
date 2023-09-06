ba8804d17286e3422eebdafcf6273d2c82225b24
mustaphasa <mostaphasa@gmail.com>
2023-09-07 00:29:39 +0100

    editting text

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           {/* Add meta description and title for SEO */}
           <meta name="description" content="Download high-quality thumbnails from YouTube videos." />
-          <title>Youtube Thumbnail Downloader</title>
+          <title>YouTube Thumbnail Downloader</title>
 
           {/* Add favicon */}
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
@@ -16,12 +16,16 @@ class MyDocument extends Document {
           <link rel="manifest" href="/favicon/site.webmanifest" />
 
           {/* Add AdSense script */}
-          <script data-ad-client="YOUR_AD_CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script
+            data-ad-client="YOUR_AD_CLIENT_ID"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
         </Head>
         <body>
           {/* Header Section */}
           <header className="bg-blue-500 text-white p-4">
-            <h1 className="text-2xl font-bold">Youtube Thumbnail Downloader</h1>
+            <h1 className="text-2xl font-bold">YouTube Thumbnail Downloader</h1>
             <p>Download high-quality thumbnails from YouTube videos.</p>
           </header>
 
@@ -30,7 +34,7 @@ class MyDocument extends Document {
 
           {/* Footer Section */}
           <footer className="bg-gray-800 text-white text-center p-4">
-            &copy; {new Date().getFullYear()} Youtube Thumbnail Downloader
+            &copy; {new Date().getFullYear()} YouTube Thumbnail Downloader
           </footer>
 
           {/* Additional Text Content */}
@@ -50,6 +54,7 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
 
 
 
