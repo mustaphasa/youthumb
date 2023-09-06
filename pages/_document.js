1f6a0c1769f0de4f976d575955f406f1701ef7c6
mustaphasa <mostaphasa@gmail.com>
2023-09-07 00:49:53 +0100

    editting text

--- pages/_document.js
+++ pages/_document.js
@@ -39,7 +39,23 @@ class MyDocument extends Document {
 
           {/* Additional Text Content */}
           <div style={{ flex: 1 }} className="bg-gray-200 p-4 text-center">
+            <p>It will always find the thumbnails in the highest quality and with max resolution.</p>
+            <p>To get the thumbnail, you need to copy the YouTube video URL into our tool.</p>
+
+            {/* Add a Call to Action (CTA) */}
+            <p>Want to get started? Simply paste the YouTube video URL above and hit the "Download Thumbnails" button below.</p>
+
             {/* Your additional text content here */}
+            <p>Save YT Video Thumbnails</p>
+            <p>Get free thumbnail image of any YouTube videos in Full HD(1080), HD (720), SD, and also in small size. it's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos</p>
+
+            <p>What is the use of this YouTube Thumbnail Grabber website?</p>
+            <p>People use this YouTube thumbnail downloader website for getting thumbnail from any YouTube videos. That can be used in presentation, animation work or many other activities.</p>
+
+            {/* ... Add more paragraphs ... */}
+
+            <p>Is reusing YouTube thumbnails are SEO friendly?</p>
+            <p>Nope it's not, in short answer it's not SEO friendly because almost all thumbnails which are on YouTube are indexed by Google. So which one is unique and which one is duplicate google know it. So it's not SEO friendly as you know. But you can do some stuff on that thumbnail I mean getting some unique effects using Photoshop or any other software that can make your thumbnail unique again so that can be reused and make that SEO friendly again.</p>
           </div>
 
           <NextScript />
@@ -50,6 +66,7 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
 
 
 
